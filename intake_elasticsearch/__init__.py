b299e215eebb4e5292e91bd8cf34e700ae63ba05
Dan Allan <dallan@bnl.gov>
2020-02-04 15:16:14 -0500

    Add support for new driver discovery mechanism.

--- intake_elasticsearch/__init__.py
+++ intake_elasticsearch/__init__.py
@@ -3,5 +3,7 @@ from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
 
+import intake  # Import this first to avoid circular imports during discovery.
+del intake
 from .elasticsearch_seq import ElasticSearchSeqSource
 from .elasticsearch_table import ElasticSearchTableSource
